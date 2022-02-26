a606bdd832e1186128ea00472f420abecad727dc
LstarXYX <294554846@qq.com>
2022-02-26 15:46:43 +0800

    瀑布流和导航栏修改

---  pokemonBook/pages/index/index.js
+++  pokemonBook/pages/index/index.js
@@ -4,11 +4,81 @@ const app = getApp()
 
 Page({
   data: {
-    current: 'main'
+    itemList:[
+      {
+      image: '../../images/book.png',
+      title: '显瘦中长款系带风衣',
+      describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+      count: '888',
+      delCount: '666'
+    },{
+      image: '../../images/book.png',
+      title: '显瘦中长款系带风衣',
+      describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+      count: '888',
+      delCount: '666'
+    },{
+      image: '../../images/book.png',
+      title: '显瘦中长款系带风衣',
+      describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+      count: '888',
+      delCount: '666'
+    },{
+      image: '../../images/book.png',
+      title: '显瘦中长款系带风衣',
+      describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+      count: '888',
+      delCount: '666'
+    },{
+      image: '../../images/book.png',
+      title: '显瘦中长款系带风衣',
+      describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+      count: '888',
+      delCount: '666'
+    },{
+      image: '../../images/book.png',
+      title: '显瘦中长款系带风衣',
+      describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+      count: '888',
+      delCount: '666'
+    },{
+      image: '../../images/book.png',
+      title: '显瘦中长款系带风衣',
+      describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+      count: '888',
+      delCount: '666'
+    },{
+      image: '../../images/book.png',
+      title: '显瘦中长款系带风衣',
+      describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+      count: '888',
+      delCount: '666'
+    },{
+      image: '../../images/book.png',
+      title: '显瘦中长款系带风衣',
+      describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+      count: '888',
+      delCount: '666'
+    }
+  
+  ]
   },
-  handleChange ({ detail }) {
-    this.setData({
-        current: detail.key
-    });
-}
+  //首次加载数据
+  onLoad(options) {
+    // 从缓存获取数据 如果没有 请求一次性加载(数据量不大)
+      var cacheList = wx.getStorageSync('itemList');
+      if(cacheList != null && cacheList != "")
+      {
+        cacheList = JSON.parse(cacheList);
+        this.itemList = cacheList;
+      }
+      else
+      {
+          //请求数据
+      }
+      
+      wx.lin.renderWaterFlow(this.data.itemList, false ,()=>{
+        console.log('渲染成功')
+      })
+  }
 })
---  pokemonBook/pages/main/main.js
+++  pokemonBook/pages/main/main.js
@@ -5,11 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
-  },
 
-  scrollToTop() {
-    console.log("aaa");
   },
 
   /**
